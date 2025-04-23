59bd2170402ef5c70b28411fd94581a03951d19c
teolhyn <teemu.hynna@gmail.com>
2025-04-23 15:45:45 +0300

    feat: more complete UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,16 @@
 'use client'
 
-import React, { useState } from 'react'
+import React from 'react'
 import FileDropZone from '@/components/FileUpload'
-
-type ConsumptionEntry = {
-  timestamp: Date
-  consumption: number
-}
 
 export default function HomePage() {
 
-  const handleParsedData = async (data: ConsumptionEntry[]) => {
-  }
-
   return (
     <main>
-      <div className='flex flex-col items-center justify-center mt-20'>
-        <h1>Kumpi sähkö?</h1>
-        <FileDropZone onFileParsed={handleParsedData} />
+      <div className='flex flex-col items-center justify-center mt-20 font-sans'>
+        <h1 className='text-7xl mb-10 text-gray-200 max-w-96 sm:max-w-screen font-extralight font-dm-sans'>Kumpi sähkö?</h1>
+        <p className='text-xl mb-5 max-w-96 text-gray-400'>Oletko miettinyt kannattaisiko sinun ostaa pörssi- vai kiinteähintainen sopimus? Ota asiasta helposti selvää!</p>
+        <FileDropZone />
       </div>
     </main>
   )

7ccc56de46c5ab0d9b5a4c2a188af5294f379aab
teolhyn <teemu.hynna@gmail.com>
2025-05-11 19:43:44 +0300

    feat: proper favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const dmSans = DM_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "Sähkönkulutuksen hintalaskuri – Pörssi vs. kiinteä",
+  title: "Sähkönkulutuksen hintalaskuri | Pörssi vs. kiinteä",
   description: "Laske sähkökustannuksesi pörssihinnoilla ja kiinteällä hinnalla suoraan kulutusdatastasi.",
   keywords: [
     "sähkön hinta",
@@ -82,7 +82,11 @@ export default function RootLayout({
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className={`${dmSans.variable} antialiased font-dm`}>
 
